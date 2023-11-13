025775957290ed322ff9bf7291be11501e79ec8b
yunjichoi9151 <cyjcode99@gmail.com>
2023-11-14 00:01:28 +0900

    📌 Init: router 설정 추가
    1. router 초기 세팅

---
+++ src/router/routes.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from 'react-router-dom';
+// import MainPage from "../pages/MainPage";
+
+const router = createBrowserRouter([
+  // {
+  //   path: "",
+  //   element: <LandingPage />,
+  // },
+  // {
+  //   path: "main",
+  //   element: <MainPage />,
+  // },
+]);
+
+export default router;
