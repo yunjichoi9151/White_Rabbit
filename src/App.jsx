f2f51a891822ceb50455d6b433a7fba06be0de2a
yunjichoi9151 <cyjcode99@gmail.com>
2023-11-16 16:56:20 +0900

    🎯 Fix: App 설정 수정
    1. globalStyle 적용
    2. router 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { GlobalStyle } from './styles/GlobalStyles';
+import { RouterProvider, Routes } from 'react-router-dom';
+import router from './router/routes';
 import BasicText from './components/common/BasicText';
 import BasicImage from './components/common/BasicImage';
 import BasicInput from './components/common/BasicInput';
@@ -10,20 +13,24 @@ const App = () => {
   };
 
   return (
-    <div>
-      <div>
-        <div>hello!</div>
-        <BasicText text='hihi' size='30px' />
-        <BasicImage src='/assets/img/back.jpg' size='5rem' />
-        <BasicInput
-          id='num'
-          type='string'
-          value={name}
-          handleOnChangeValue={changeData}
-          placeholder='입력하세요'
-        />
-      </div>
-    </div>
+    // <div>
+    //   <div>
+    //     <div>hello!</div>
+    //     <BasicText text='hihi' size='30px' />
+    //     <BasicImage src='/assets/img/back.jpg' size='5rem' />
+    //     <BasicInput
+    //       id='num'
+    //       type='string'
+    //       value={name}
+    //       handleOnChangeValue={changeData}
+    //       placeholder='입력하세요'
+    //     />
+    //   </div>
+    // </div>
+    <>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </>
   );
 };
 
