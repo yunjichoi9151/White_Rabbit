35f75481e88756327e745b2eec6c0f20009bf612
yunjichoi9151 <cyjcode99@gmail.com>
2023-11-16 16:55:13 +0900

    ✅ Test: router test
    1. router 이동 테스트

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,7 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 // import MainPage from "../pages/MainPage";
+import Home from '../pages/Home';
+import React from 'react';
 
 const router = createBrowserRouter([
+  {
+    path: '/home',
+    element: <Home />,
+  },
   // {
   //   path: "",
   //   element: <LandingPage />,
