228d1d6f54d600d0acb93d9c8eb61b0604430d00
given02 <votmdnjem_id@naver.com>
2023-11-16 16:20:11 +0900

    💄 Style: themeProvider 적용
    1. themeProvider 적용
    2. theme.jsx 추가

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
 import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
 );
