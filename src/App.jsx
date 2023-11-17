27f688ef1564c9f2424eeb85a2af91d140cf8c1d
given02 <votmdnjem_id@naver.com>
2023-11-18 00:08:55 +0900

    ♻️ Refactor: 프로필 컴포넌트 하나로 통합
    1. 프로필 컴포넌트 하나로 통합

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
 import { GlobalStyle } from './styles/GlobalStyles';
 import { RouterProvider, Routes } from 'react-router-dom';
-import ProfileDetail from './components/profile/ProfileDetail';
-import ProfileTag from './components/profile/ProfileTag';
-import TabBar from './components/profile/TabBar';
+import ProfileTag from './components/profile/ProfileBar';
 import router from './router/routes';
 
 const App = () => {
@@ -11,30 +9,22 @@ const App = () => {
     <>
       <GlobalStyle />
       <RouterProvider router={router} />
-      <TabBar
-        texts={['팔로워', '팔로잉']}
-        existCounter={[true, true]}
-        countNum={[30, 30]}
-      />
-      {/* <ProfileTag
-        name="hi"
+      <ProfileTag
+        name="김엘리스"
         rate="레이서"
+        existGeneration={true}
+        genType="SW 엔지니어 트랙"
+        genNum="6기"
+        existTimeAgo={false}
         createdAt="2023-11-17 20:59:00"
         existFollow={true}
-        isFollow={true}
-        existMore={true}
-      /> */}
-      {/* <ProfileDetail
-        name={'김엘리스'}
-        genType={'SW 엔지니어 트랙'}
-        genNum={'6기'}
-        rate={'레이서'}
         followers={30}
         followings={30}
-        existFollow={true}
-        isFollow={false}
-        isList={false}
-      /> */}
+        existFollowBtn={true}
+        isFollow={true}
+        existMoreBtn={false}
+        profileSize={3}
+      />
     </>
   );
 };
--- src/components/profile/ProfileBar/style.jsx
+++ src/components/profile/ProfileBar/style.jsx
@@ -4,6 +4,7 @@ export const Container = styled.div`
   width: ${({ width }) => width || '100%'};
   height: ${({ height }) => height || '100%'};
   display: flex;
+  padding: ${({ padding }) => padding || 0};
   flex-direction: ${({ direction }) => direction};
   justify-content: ${({ direction }) =>
     direction === 'column' ? 'center' : 'flex-start'};
