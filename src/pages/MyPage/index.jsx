dd500a9b55e6b2861aaff52f0b618c1716c411dd
yunjichoi9151 <cyjcode99@gmail.com>
2023-11-17 17:20:13 +0900

    📌 Init: MyPage 추가
    1. MyPage 추가

---
+++ src/pages/MyPage/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as S from './style';
+import NavBar from '../../components/common/NavBar';
+
+const MyPage = () => {
+  return (
+    <S.MyPageWrap>
+      <div>MyPage</div>
+      <NavBar />
+    </S.MyPageWrap>
+  );
+};
+
+export default MyPage;
