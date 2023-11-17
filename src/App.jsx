56feb362fef925ee58d4000d5c254a9141b61097
given02 <votmdnjem_id@naver.com>
2023-11-18 00:10:13 +0900

    ♻️ Refactor: 테스트용 코드 제거
    1. 테스트용 코드 제거

--- src/App.jsx
+++ src/App.jsx
@@ -9,22 +9,6 @@ const App = () => {
     <>
       <GlobalStyle />
       <RouterProvider router={router} />
-      <ProfileTag
-        name="김엘리스"
-        rate="레이서"
-        existGeneration={true}
-        genType="SW 엔지니어 트랙"
-        genNum="6기"
-        existTimeAgo={false}
-        createdAt="2023-11-17 20:59:00"
-        existFollow={true}
-        followers={30}
-        followings={30}
-        existFollowBtn={true}
-        isFollow={true}
-        existMoreBtn={false}
-        profileSize={3}
-      />
     </>
   );
 };
