edae634aeda6eee72c3a68ab9bbdb6b87b93bf26
yunjichoi9151 <cyjcode99@gmail.com>
2023-11-17 22:42:05 +0900

    🎯 Fix: GlobalStyles 위치 변경
    1. import 위치 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
 import React, { useState } from 'react';
-import { GlobalStyle } from './styles/GlobalStyles';
 import { RouterProvider, Routes } from 'react-router-dom';
 import router from './router/routes';
 
 const App = () => {
-  const texts = ['팔로워', '팔로잉'];
-  const isCountables = [true, true];
-
-  const [name, setName] = useState('');
-  const changeData = (e) => {
-    setName(e.target.value);
-  };
-
-  const [currentTab, setCurrentTab] = useState('0');
-  const handleTabClick = (tabKey) => {
-    setCurrentTab(tabKey);
-  };
-
   return (
     <>
-      <GlobalStyle />
       <RouterProvider router={router} />
     </>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
 import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { GlobalStyle } from './styles/GlobalStyles';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <StrictMode>
+    <GlobalStyle />
     <App />
   </StrictMode>,
 );
