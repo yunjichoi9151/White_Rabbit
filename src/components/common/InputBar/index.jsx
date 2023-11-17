5c22839eb35b2fa08b3375143d0259bedfde3d9c
yunjichoi9151 <cyjcode99@gmail.com>
2023-11-17 17:14:09 +0900

    📌 Init: InputBar 생성
    1. InputBar 생성

---
+++ src/components/common/InputBar/index.jsx
@@ -0,0 +1,35 @@
+import * as S from './style';
+import React from 'react';
+import BasicInput from '../BasicInput';
+
+const InputBar = ({
+  id,
+  type,
+  value,
+  handleOnChangeValue,
+  placeholder = '',
+  isReadOnly = false,
+  inputStyle,
+  inputBarStyle,
+  children,
+  existLeft = false,
+  existRight = false,
+}) => {
+  return (
+    <S.InputBar style={inputBarStyle}>
+      {existLeft && children}
+      <BasicInput
+        id={id}
+        type={type}
+        value={value}
+        onChange={handleOnChangeValue}
+        placeholder={placeholder}
+        readOnly={isReadOnly}
+        style={inputStyle}
+      />
+      {existRight && children}
+    </S.InputBar>
+  );
+};
+
+export default InputBar;
