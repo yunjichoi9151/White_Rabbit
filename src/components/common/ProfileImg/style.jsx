2fa7582c9b665bb61c04415b3e75e7d67bb705d3
yunjichoi9151 <cyjcode99@gmail.com>
2023-11-17 17:14:56 +0900

    📌 Init: ProfileImg 생성
    1. ProfileImg 생성

---
+++ src/components/common/ProfileImg/style.jsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const ProfileImage = styled.div`
+  width: 100%;
+  border-radius: 50%;
+  border: 0.125rem solid var(--color-primary);
+  aspect-ratio: 1 / 1;
+
+  background-size: cover;
+  background-image: ${(props) => `url(${props.src})`};
+  background-position: center center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
+export const EditIconWrapper = styled.div`
+  display: flex;
+  position: absolute;
+  width: 2.5rem;
+  height: 2.5rem;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 50%;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const EditIcon = styled.input`
+  position: absolute;
+  width: 0;
+  height: 0;
+  padding: 0;
+  overflow: hidden;
+  display: none;
+  border: 0;
+`;
