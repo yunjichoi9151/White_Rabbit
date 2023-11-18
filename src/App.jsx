a7ecfc5e55c33d6f2022233618b020380b43efe9
given02 <votmdnjem_id@naver.com>
2023-11-19 00:38:23 +0900

    🔥 Remove: 사용안하는 import문 제거
    1. 사용안하는 import문 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import { RouterProvider, Routes } from 'react-router-dom';
-import Post from './components/board/Post';
-import TabBar from './components/profile/TabBar';
 import router from './router/routes';
 
 const App = () => {
