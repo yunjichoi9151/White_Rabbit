6f0d157d64cf6bd2e8f101eecd41c8d2a4da75df
Jiwon <hajw.study@gmail.com>
2023-11-20 10:16:57 +0900

    🎨 Design: Landing 페이지 UI
    Landing 페이지 UI 작업

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { RouterProvider, Routes } from 'react-router-dom';
 import router from './router/routes';
+import SkillLinkPage from './pages/SkillLinkPage';
+import LinkIntro from './components/SkillLinkIntro/LinkIntro';
 
 const App = () => {
   return (
