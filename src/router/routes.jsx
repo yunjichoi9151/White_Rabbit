0274807201250dee0a28af74c4a15eb34d897a04
yunjichoi9151 <cyjcode99@gmail.com>
2023-11-21 20:48:00 +0900

    ✨ Feat: router 설정 추가
    1. 새로운 페이지 관련 설정 추가

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -9,9 +9,11 @@ import LinkEdit from '../pages/IntroLinkEdit';
 import NewLink from '../pages/IntroNewLink';
 import Join from '../pages/Join';
 import FindPW from '../pages/FindPW';
-import NewSkill from '../pages/IntroNewSkill';
+// import NewSkill from '../pages/IntroNewSkill';
 import RootContainer from '../pages/RootContainer/RootContainer';
-import ProfileEdit from '../pages/ProfileEdit';
+// import ProfileEdit from '../pages/ProfileEdit';
+import Detail from '../pages/Detail';
+import Write from '../pages/Write';
 
 export const ROUTER_LINK = {
   LANDING: { path: '/', link: '/' },
@@ -23,8 +25,10 @@ export const ROUTER_LINK = {
   NEWLINK: { path: '/newlink', link: '/newlink' },
   JOIN: { path: '/join', link: '/join' },
   FINDPW: { path: '/findpw', link: '/findpw' },
-  NEWSKILL: { path: '/newskill', link: '/newskill' },
-  PROFILEEDIT: { path: '/profileedit', link: '/profileedit' },
+  // NEWSKILL: { path: '/newskill', link: '/newskill' },
+  // PROFILEEDIT: { path: '/profileedit', link: '/profileedit' },
+  DETAIL: { path: '/post/:postId', link: '/post' },
+  WRITE: { path: '/write', link: '/write' },
 };
 
 const router = createBrowserRouter([
@@ -69,13 +73,21 @@ const router = createBrowserRouter([
         element: <FindPW />,
       },
       {
-        path: ROUTER_LINK.NEWSKILL.path,
-        element: <NewSkill />,
+        path: ROUTER_LINK.DETAIL.path,
+        element: <Detail />,
       },
       {
-        path: ROUTER_LINK.PROFILEEDIT.path,
-        element: <ProfileEdit />,
+        path: ROUTER_LINK.WRITE.path,
+        element: <Write />,
       },
+      // {
+      //   path: ROUTER_LINK.NEWSKILL.path,
+      //   element: <NewSkill />,
+      // },
+      // {
+      //   path: ROUTER_LINK.PROFILEEDIT.path,
+      //   element: <ProfileEdit />,
+      // },
     ],
   },
 ]);
