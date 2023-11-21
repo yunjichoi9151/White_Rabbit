0fa0f9d527d5030dc33dbcec0620058ddb63ae3b
yunjichoi9151 <cyjcode99@gmail.com>
2023-11-21 20:53:02 +0900

    ✨ Feat: Home 기능 추가
    1. Home 기능 추가

--- src/pages/Home/style.jsx
+++ src/pages/Home/style.jsx
@@ -1,6 +1,28 @@
 import styled from 'styled-components';
+import * as CS from '../../styles/CommonStyles';
 
 export const HomeWrap = styled.div`
   width: 100%;
-  height: 100%;
+  background-color: ${CS.color.contentTertiary};
+`;
+
+export const TopBtnWrap = styled.div`
+  width: 100%;
+  max-width: 900px;
+  height: 3rem;
+  padding: 0.5rem 1rem;
+  background-color: ${CS.color.white};
+  display: flex;
+  position: fixed;
+  z-index: 999;
+`;
+
+export const BoardWrap = styled.div`
+  width: 100%;
+  max-width: 900px;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  padding-top: 3.5rem;
+  padding-bottom: 6.75rem;
 `;
