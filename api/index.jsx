35e8c8aa4bb4fbee3a19b189107ba4f2b1758096
yunjichoi9151 <cyjcode99@gmail.com>
2023-11-22 20:46:32 +0900

    🎯 Fix: BASE_URL 변경
    1. BASE_URL 배포된 URL로 변경

--- api/index.jsx
+++ api/index.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BASE_URL = 'http://kdt-sw-6-team07.elicecoding.com/api/v1';
-const BASE_URL = 'http://localhost:5000/api/v1';
+const BASE_URL = 'http://kdt-sw-6-team07.elicecoding.com/api/v1';
+// const BASE_URL = 'http://localhost:5000/api/v1';
 const instance = axios.create({
   baseURL: BASE_URL,
   headers: {
