88596702bee0e0fa6694f3e6b50b8fa36569b81f
given02 <votmdnjem_id@naver.com>
2023-11-30 00:50:33 +0900

    🚧 Progress: Generation post api 추후 수정 필요

--- api/utils/Generation.jsx
+++ api/utils/Generation.jsx
@@ -2,7 +2,7 @@ import { api } from '../index';
 // API 수정 필요
 export const generationApi = {
   // 기수 추가
-  postFollow: () => api.post(`/generations/${toUserId}`),
+  //   postFollow: () => api.post(`/generations`),
 
   // 기수 전체 조회
   getAllGenerations: () => api.get(`/generations`),
