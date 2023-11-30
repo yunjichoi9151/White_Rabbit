be7f94b77412fd2234c32d3e9464e7881bce2a9a
yunjichoi9151 <cyjcode99@gmail.com>
2023-11-30 22:38:31 +0900

    🎯 Fix: BasicInput ClearBtn 추가
    1. ClearBtn 옵션 추가

--- src/components/common/BasicInput/index.jsx
+++ src/components/common/BasicInput/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import * as S from './style';
+import * as CS from '../../../styles/CommonStyles';
+import BasicButton from '../BasicButton';
+import { RiDeleteBack2Fill } from 'react-icons/ri';
 
 const BasicInput = ({
   id,
@@ -11,25 +14,39 @@ const BasicInput = ({
   onKeyDown,
   placeholder = '',
   isReadOnly = false,
+  existClearBtn = false,
+  btnOnClick,
   style,
   name,
   ...rest
 }) => {
   return (
-    <S.Input
-      id={id}
-      src={src}
-      type={type}
-      value={value}
-      accept={accept}
-      onChange={onChange}
-      onKeyDown={onKeyDown}
-      placeholder={placeholder}
-      readOnly={isReadOnly}
-      style={style}
-      name={name}
-      {...rest}
-    />
+    <>
+      <S.Input
+        id={id}
+        src={src}
+        type={type}
+        value={value}
+        accept={accept}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        placeholder={placeholder}
+        readOnly={isReadOnly}
+        style={style}
+        name={name}
+        {...rest}
+      />
+      <BasicButton
+        handleOnClickButton={btnOnClick}
+        existIcon={true}
+        btnStyle={{
+          width: '2rem',
+          display: existClearBtn ? 'block' : 'none',
+        }}
+      >
+        <RiDeleteBack2Fill color={CS.color.black} size="1.5rem" />
+      </BasicButton>
+    </>
   );
 };
 
