fe5a1e40ce48d6d8fe4965d7ee965a60e6206c83
yunjichoi9151 <cyjcode99@gmail.com>
2023-12-02 02:49:34 +0900

    🎯 Fix: ref 추가
    1. ref 추가

--- src/components/common/BasicInput/index.jsx
+++ src/components/common/BasicInput/index.jsx
@@ -1,53 +1,59 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import * as S from './style';
 import * as CS from '../../../styles/CommonStyles';
 import BasicButton from '../BasicButton';
 import { RiDeleteBack2Fill } from 'react-icons/ri';
 
-const BasicInput = ({
-  id,
-  src,
-  type,
-  value,
-  accept,
-  onChange,
-  onKeyDown,
-  placeholder = '',
-  isReadOnly = false,
-  existClearBtn = false,
-  btnOnClick,
-  style,
-  name,
-  ...rest
-}) => {
-  return (
-    <>
-      <S.Input
-        id={id}
-        src={src}
-        type={type}
-        value={value}
-        accept={accept}
-        onChange={onChange}
-        onKeyDown={onKeyDown}
-        placeholder={placeholder}
-        readOnly={isReadOnly}
-        style={style}
-        name={name}
-        {...rest}
-      />
-      <BasicButton
-        handleOnClickButton={btnOnClick}
-        existIcon={true}
-        btnStyle={{
-          width: '2rem',
-          display: existClearBtn ? 'block' : 'none',
-        }}
-      >
-        <RiDeleteBack2Fill color={CS.color.black} size="1.5rem" />
-      </BasicButton>
-    </>
-  );
-};
+const BasicInput = forwardRef(
+  (
+    {
+      id,
+      src,
+      type,
+      value,
+      accept,
+      onChange,
+      onKeyDown,
+      placeholder = '',
+      isReadOnly = false,
+      existClearBtn = false,
+      btnOnClick,
+      style,
+      name,
+      ...rest
+    },
+    ref,
+  ) => {
+    return (
+      <>
+        <S.Input
+          id={id}
+          src={src}
+          type={type}
+          value={value}
+          accept={accept}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          placeholder={placeholder}
+          readOnly={isReadOnly}
+          style={style}
+          name={name}
+          ref={ref}
+          {...rest}
+        />
+        <BasicButton
+          handleOnClickButton={btnOnClick}
+          existIcon={true}
+          btnStyle={{
+            width: '2rem',
+            display: existClearBtn ? 'block' : 'none',
+          }}
+        >
+          <RiDeleteBack2Fill color={CS.color.black} size="1.5rem" />
+        </BasicButton>
+      </>
+    );
+  },
+);
 
 export default BasicInput;
--- src/components/common/InputBar/index.jsx
+++ src/components/common/InputBar/index.jsx
@@ -1,45 +1,51 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import * as S from './style';
 import BasicInput from '../BasicInput';
 
-const InputBar = ({
-  id,
-  src,
-  type,
-  value,
-  accept,
-  handleOnChangeValue,
-  handleOnKeyDownValue,
-  placeholder = '',
-  isReadOnly = false,
-  inputStyle,
-  inputBarStyle,
-  children,
-  existLeft = false,
-  existRight = false,
-  existClearBtn,
-  btnOnClick,
-}) => {
-  return (
-    <S.InputBar style={inputBarStyle}>
-      {existLeft && children}
-      <BasicInput
-        id={id}
-        type={type}
-        value={value}
-        accept={accept}
-        src={src}
-        onChange={handleOnChangeValue}
-        onKeyDown={handleOnKeyDownValue}
-        placeholder={placeholder}
-        readOnly={isReadOnly}
-        style={inputStyle}
-        existClearBtn={existClearBtn}
-        btnOnClick={btnOnClick}
-      />
-      {existRight && children}
-    </S.InputBar>
-  );
-};
+const InputBar = forwardRef(
+  (
+    {
+      id,
+      src,
+      type,
+      value,
+      accept,
+      handleOnChangeValue,
+      handleOnKeyDownValue,
+      placeholder = '',
+      isReadOnly = false,
+      inputStyle,
+      inputBarStyle,
+      children,
+      existLeft = false,
+      existRight = false,
+      existClearBtn,
+      btnOnClick,
+    },
+    ref,
+  ) => {
+    return (
+      <S.InputBar style={inputBarStyle}>
+        {existLeft && children}
+        <BasicInput
+          id={id}
+          type={type}
+          value={value}
+          accept={accept}
+          src={src}
+          onChange={handleOnChangeValue}
+          onKeyDown={handleOnKeyDownValue}
+          placeholder={placeholder}
+          readOnly={isReadOnly}
+          style={inputStyle}
+          existClearBtn={existClearBtn}
+          btnOnClick={btnOnClick}
+          ref={ref}
+        />
+        {existRight && children}
+      </S.InputBar>
+    );
+  },
+);
 
 export default InputBar;
