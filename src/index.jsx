e14b4bf7a45b6d56f0c519186cc96f8f052a61a9
yunjichoi9151 <cyjcode99@gmail.com>
2023-12-01 22:59:49 +0900

    📌 Init: service worker 추가
    1. service worker 관련 설정 추가

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { GlobalStyle } from './styles/GlobalStyles';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+serviceWorkerRegistration.register();
 root.render(
   <>
     <GlobalStyle />
