203e510bddb5dd695261eb65499a53a2b5fd2aa2
given02 <votmdnjem_id@naver.com>
2023-12-02 01:06:42 +0900

    ✨ Feat: 팔로우 목록에서 팔로우 버튼 수정

--- src/pages/Follow/index.jsx
+++ src/pages/Follow/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate, useParams, useLocation } from 'react-router';
 import { useEffect } from 'react';
 import { userApi } from '../../../api/utils/user';
-import { Link } from 'react-router-dom';
+import { followApi } from '../../../api/utils/Follow';
 import { ROUTER_LINK } from '../../router/routes';
 import * as S from './style';
 import * as CS from '../../styles/CommonStyles';
@@ -10,44 +10,73 @@ import Header from '../../components/common/Header';
 import TabBar from '../../components/profile/TabBar';
 import ProfileBar from '../../components/profile/ProfileBar';
 
+const userLabelText = {
+  USER: '레이서',
+  ADMIN: '관리자',
+  COACH: '코치',
+};
+
 function Follow() {
+  const navigate = useNavigate();
   const location = useLocation();
-
-  const [followData, setFollowData] = useState({
-    follower: [],
-    following: [],
-  });
-
   const { state } = location;
-
   const { type } = useParams();
 
+  // tab
   const [tabName, setTabName] = useState(
     type === 'follower' ? 'follower' : 'following',
   );
-
-  const navigate = useNavigate();
 
   const handleClickTab = (tabKey) => {
     setTabName(tabKey);
   };
 
+  // loginUser
+  const [loginUser, setLoginUser] = useState([]);
+
+  const getLoginUserInfo = async () => {
+    try {
+      const res = await userApi.getLoginUserInfo();
+      setLoginUser(res.data.data);
+      console.log('loginUser:', res.data.data);
+    } catch (error) {
+      console.log('error: ', error);
+    }
+  };
+
+  // followData
+  const [followData, setFollowData] = useState({
+    follower: [],
+    following: [],
+  });
+
   const getFollowList = async () => {
     const response = await userApi.followList(state._id);
-
     const { follower, following } = response.data;
 
     setFollowData({
       follower,
       following,
     });
+
+    console.log('followData:', response.data);
   };
 
-  const [loginUser, setLoginUser] = useState([]);
-  const getLoginUserInfo = async () => {
+  // follow click
+  const handleOnClickFollow = async (followerInfo) => {
     try {
-      const res = await userApi.getLoginUserInfo();
-      setLoginUser(res.data.data);
+      if (followerInfo.is_follow) {
+        const response = await followApi.deleteFollowById(followerInfo._id);
+
+        if (response.status === 200) {
+          getFollowList();
+        }
+      } else {
+        const response = await followApi.postFollow(followerInfo._id);
+        if (response.status === 201) {
+          getFollowList();
+        }
+      }
     } catch (error) {
       console.log('error: ', error);
     }
@@ -57,12 +86,6 @@ function Follow() {
     getLoginUserInfo();
     getFollowList();
   }, []);
-
-  const userLabelText = {
-    USER: '레이서',
-    ADMIN: '관리자',
-    COACH: '코치',
-  };
 
   return (
     <>
@@ -87,10 +110,7 @@ function Follow() {
       </S.TabWrap>
 
       {followData[tabName].map((follower, index) => (
-        <Link
-          key={`${follower._id}_${index}`}
-          to={`${ROUTER_LINK.USERPAGE.link}/${follower._id}`}
-        >
+        <>
           <ProfileBar
             username={follower.name}
             rate={`${userLabelText[follower.roles]}`}
@@ -101,6 +121,8 @@ function Follow() {
             isEditable={false}
             profileSize={2}
             existFollowBtn={loginUser._id !== follower._id}
+            isFollow={follower.is_follow}
+            handleOnClickFollow={() => handleOnClickFollow(follower)}
             style={{
               marginTop: 20,
               marginLeft: 20,
@@ -108,11 +130,14 @@ function Follow() {
               paddingRight: 40,
               height: 'auto',
             }}
-            isFollow={follower.is_follow}
-            followId={follower.followId}
+            handleOnClickBar={() =>
+              navigate(
+                ROUTER_LINK.USERPAGE.path.replace(':userId', follower._id),
+              )
+            }
           />
           <S.UnderLine />
-        </Link>
+        </>
       ))}
     </>
   );
